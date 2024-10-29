836672aaa750b1d1184316306f26777bf336d8b6
nubrixsecurity <142175078+nubrixsecurity@users.noreply.github.com>
2024-10-29 04:19:48 -0500

    Add files via upload

---
+++ Connection/Connect-Modules.ps1
@@ -0,0 +1,20 @@
+Write-Host "Attempting to connect to Exchange Online..." -ForegroundColor Yellow
+try{
+    Connect-ExchangeOnline -ShowBanner:$false
+    Write-Host "Connected to Exchange Online successfully!" -ForegroundColor Green
+
+    try {
+        Write-Host "Connecting to Microsoft Graph" -ForegroundColor Yellow
+        $scopes = @("Group.ReadWrite.All", "User.ReadWrite.All", "Directory.ReadWrite.All")
+        Connect-MgGraph -Scopes $scopes
+
+        Write-Host "Connected to Microsoft Graph successfully!" -ForegroundColor Green
+
+    } catch {
+        Write-Host "Failed to connect to Microsoft Graph." -ForegroundColor Red
+        exit 1
+    }
+} catch {
+    Write-Host  "Failed to connect to Exchange Online." -ForegroundColor Red
+    exit 1
+}
